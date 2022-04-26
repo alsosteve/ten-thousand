a80695493d8acc692d532cd5f48a52dd1a86fafe
Steve Ngo <stevehngo@hotmail.com>
2022-04-25 20:01:56 -0400

    started on score func

--- ten_thousand/game_logic.py
+++ ten_thousand/game_logic.py
@@ -6,3 +6,27 @@ class GameLogic:
         return tuple(randint(1,6) for _ in range(0,num_dice))
         # or
         # return tuple(sample(range(1, 6 + 1), num_dice))
+
+    @staticmethod
+    def calculate_score(dice_list):
+        score = 0
+        if 1 in dice_list:
+            return "yes"
+
+    '''
+    def calculate_score(dice_list):
+        total = 0
+        for item in score:
+            times = score.count(item)
+            if times == 2:
+                if item == 1:
+                    total += 200
+                if item == 5:
+                    total += 100
+            if times == 1:
+                if item == 1:
+                    total += 100
+                if item == 5:
+                    total += 50
+        return total
+    '''
--- tests/test_calculate_score.py
+++ tests/test_calculate_score.py
@@ -3,31 +3,31 @@ from ten_thousand.game_logic import GameLogic
 
 pytestmark = [pytest.mark.version_1]
 
-@pytest.mark.skip("pending")
+
 def test_single_five():
     actual = GameLogic.calculate_score((5,))
     expected = 50
     assert actual == expected
 
-@pytest.mark.skip("pending")
+
 def test_single_one():
     actual = GameLogic.calculate_score((1,))
     expected = 100
     assert actual == expected
 
-@pytest.mark.skip("pending")
+
 def test_two_fives():
     actual = GameLogic.calculate_score((5, 5))
     expected = 100
     assert actual == expected
 
-@pytest.mark.skip("pending")
+
 def test_two_ones():
     actual = GameLogic.calculate_score((1, 1))
     expected = 200
     assert actual == expected
 
-@pytest.mark.skip("pending")
+
 def test_one_and_five():
     actual = GameLogic.calculate_score((1, 5))
     expected = 150
